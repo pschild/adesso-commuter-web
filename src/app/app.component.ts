a38be7ed128ca3ad0c91c681513aaa1609f2e9dc
Schild, Philippe <Philippe.Schild@adesso.de>
2020-10-03 16:37:38 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IMqttMessage } from 'ngx-mqtt';
+import { Subscription } from 'rxjs';
+import { EventMqttService } from './event-mqtt.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'adesso-commuter-web';
+export class AppComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription;
+
+  constructor(private eventMqttService: EventMqttService) {
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.eventMqttService.observe('devices/+/version')
+      .subscribe((data: IMqttMessage) => {
+        console.log(data.payload.toString());
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MqttModule } from 'ngx-mqtt';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MqttModule.forRoot({
+      hostname: '192.168.178.28',
+      port: 1884,
+      path: '',
+      protocol: 'ws'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
